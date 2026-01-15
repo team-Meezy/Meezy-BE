fc723161266a992e8a31dbb58cd81bb5e340fea2
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-01-15 21:53:30 +0900

    feat: Virtual Thread 도입

--- src/main/java/com/example/meezy/bc/team/chat_message/infrastructure/adapter/out/messaging/ChatMessageBrokerPublisher.java
+++ src/main/java/com/example/meezy/bc/team/chat_message/infrastructure/adapter/out/messaging/ChatMessageBrokerPublisher.java
@@ -8,24 +8,29 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChatMessageBrokerPublisher implements ChatMessagePublishPort {
 
+    private static final ExecutorService VIRTUAL_THREAD_EXECUTOR =
+            Executors.newVirtualThreadPerTaskExecutor();
+
     private final RabbitTemplate rabbitTemplate;
 
     @Override
     public CompletableFuture<Boolean> publish(ChatMessageEvent event) {
         return CompletableFuture.supplyAsync(() -> {
-            try{
+            try {
                 rabbitTemplate.convertAndSend(event);
                 return true;
-            } catch (Exception e){
+            } catch (Exception e) {
                 log.error("메시지 전송에 실패: {}", e.getMessage(), e);
                 return false;
             }
-        });
+        }, VIRTUAL_THREAD_EXECUTOR);
     }
 }
