1dda6e9f03bc426d3ba720c0da1ff1167a3e1dad
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-01-15 09:52:31 +0900

    feat: service 로직 구현

---
+++ src/main/java/com/example/meezy/bc/team/chat_room/application/service/CreateChatRoomService.java
@@ -0,0 +1,40 @@
+package com.example.meezy.bc.team.chat_room.application.service;
+
+import com.example.meezy.bc.sharedkernel.user.CurrentUserQuery;
+import com.example.meezy.bc.team.chat_room.application.service.dto.request.CreateChatRoomRequest;
+import com.example.meezy.bc.team.chat_room.domain.ChatRoom;
+import com.example.meezy.bc.team.chat_room.domain.repository.ChatRoomRepository;
+import com.example.meezy.bc.team.team.application.service.exception.TeamNotFoundException;
+import com.example.meezy.bc.team.team.domain.Team;
+import com.example.meezy.bc.team.team.domain.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CreateChatRoomService {
+
+    private final TeamRepository teamRepository; //같은 BC 간 참조 가능
+    private final ChatRoomRepository chatRoomRepository;
+    private final CurrentUserQuery currentUserQuery;
+
+    @Transactional
+    public void create(UUID teamId, CreateChatRoomRequest request){
+
+        Team team = findTeamOrThrow(teamId);
+
+        team.validateLeaderPermission(currentUserQuery.currentUser().userId());
+
+        chatRoomRepository.save(
+                ChatRoom.create(team.getTeamId(), request.name())
+        );
+    }
+
+    private Team findTeamOrThrow(UUID teamId) {
+        return teamRepository.findByTeamId_Value(teamId)
+                .orElseThrow(TeamNotFoundException::new);
+    }
+}
