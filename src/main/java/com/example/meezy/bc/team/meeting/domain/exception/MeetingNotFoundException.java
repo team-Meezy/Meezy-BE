468bb811ef4f83b2450c6f19e4c7854938852df1
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-01-17 15:55:55 +0900

    feat: exception 로직 개발

---
+++ src/main/java/com/example/meezy/bc/team/meeting/domain/exception/MeetingNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.meezy.bc.team.meeting.domain.exception;
+
+import com.example.meezy.bc.sharedkernel.exception.CustomException;
+import com.example.meezy.bc.sharedkernel.exception.ErrorCode;
+
+public class MeetingNotFoundException extends CustomException {
+
+    public MeetingNotFoundException() {
+        super(ErrorCode.MEETING_NOT_FOUND);
+    }
+}
