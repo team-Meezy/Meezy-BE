28d06b236eddf0cf20c4d60c867648462653c86c
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-01-20 08:39:49 +0900

    feat: 도메인 로직 구현

---
+++ src/main/java/com/example/meezy/bc/team/meeting_report/domain/vo/FeedbackId.java
@@ -0,0 +1,19 @@
+package com.example.meezy.bc.team.meeting_report.domain.vo;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.UUID;
+
+@Embeddable
+public record FeedbackId(
+        UUID value
+) {
+
+    public static FeedbackId newId(){
+        return new FeedbackId(UUID.randomUUID());
+    }
+
+    public static FeedbackId of(UUID feedbackId){
+        return new FeedbackId(feedbackId);
+    }
+}
