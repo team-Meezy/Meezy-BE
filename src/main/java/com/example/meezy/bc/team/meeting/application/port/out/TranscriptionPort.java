8a24418df9bba60eb875911f821b31d29be4f5cb
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-01-20 10:06:42 +0900

    refactor: 삭제

--- src/main/java/com/example/meezy/bc/team/meeting/application/port/out/TranscriptionPort.java
+++
@@ -1,8 +0,0 @@
-package com.example.meezy.bc.team.meeting.application.port.out;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface TranscriptionPort {
-
-    String transcribe(MultipartFile audio);
-}
