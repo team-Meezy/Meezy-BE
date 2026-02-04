10dbf0b1cb0dd5b658303d449275dc2f68da8b94
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-02-04 14:54:39 +0900

    refactor: import 네이밍 변경

--- src/main/java/com/example/meezy/bc/collaboration/team/application/service/team/DeleteTeamService.java
+++ src/main/java/com/example/meezy/bc/collaboration/team/application/service/team/DeleteTeamService.java
@@ -1,7 +1,7 @@
 package com.example.meezy.bc.collaboration.team.application.service.team;
 
 import com.example.meezy.bc.sharedkernel.user.CurrentUserQuery;
-import com.example.meezy.bc.collaboration.team.application.port.out.FileStoragePort;
+import com.example.meezy.bc.sharedkernel.file.FileStoragePort;
 import com.example.meezy.bc.collaboration.team.domain.Team;
 import com.example.meezy.bc.collaboration.team.application.service.exception.TeamNotFoundException;
 import com.example.meezy.bc.collaboration.team.domain.repository.TeamRepository;
--- src/main/java/com/example/meezy/bc/collaboration/team/application/service/team/UpdateTeamService.java
+++ src/main/java/com/example/meezy/bc/collaboration/team/application/service/team/UpdateTeamService.java
@@ -1,7 +1,7 @@
 package com.example.meezy.bc.collaboration.team.application.service.team;
 
 import com.example.meezy.bc.sharedkernel.user.CurrentUserQuery;
-import com.example.meezy.bc.collaboration.team.application.port.out.FileStoragePort;
+import com.example.meezy.bc.sharedkernel.file.FileStoragePort;
 import com.example.meezy.bc.collaboration.team.application.service.dto.request.UpdateServerImageRequest;
 import com.example.meezy.bc.collaboration.team.application.service.dto.request.UpdateTeamNameRequest;
 import com.example.meezy.bc.collaboration.team.domain.Team;
