70f88e3efa6d7da11de6e9406da07ff1e596224b
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-02-04 15:00:33 +0900

    feat: 컨트롤러 개발

---
+++ src/main/java/com/example/meezy/bc/user/user/presentation/ProfileController.java
@@ -0,0 +1,46 @@
+package com.example.meezy.bc.user.user.presentation;
+
+import com.example.meezy.bc.user.user.application.service.dto.request.ChangePasswordRequest;
+import com.example.meezy.bc.user.user.application.service.dto.request.WithdrawRequest;
+import com.example.meezy.bc.user.user.application.service.dto.response.MyProfileResponse;
+import com.example.meezy.bc.user.user.application.service.profile.ChangePasswordService;
+import com.example.meezy.bc.user.user.application.service.profile.QueryMyProfileService;
+import com.example.meezy.bc.user.user.application.service.profile.UploadProfileImageService;
+import com.example.meezy.bc.user.user.application.service.profile.WithdrawService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/profile")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final QueryMyProfileService queryMyProfileService;
+    private final ChangePasswordService changePasswordService;
+    private final UploadProfileImageService uploadProfileImageService;
+    private final WithdrawService withdrawService;
+
+    @GetMapping
+    public MyProfileResponse getMyProfile() {
+        return queryMyProfileService.query();
+    }
+
+    @PatchMapping("/password")
+    public void changePassword(@Valid @RequestBody ChangePasswordRequest request) {
+        changePasswordService.change(request);
+    }
+    
+
+    @PatchMapping(value = "/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void uploadProfileImage(@RequestPart MultipartFile profileImage) {
+        uploadProfileImageService.upload(profileImage);
+    }
+
+    @DeleteMapping
+    public void withdraw(@Valid @RequestBody WithdrawRequest request) {
+        withdrawService.withdraw(request);
+    }
+}
