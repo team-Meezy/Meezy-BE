edb74be9ee3e22111f2289a24dcf9f085b3db33a
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-02-11 20:01:02 +0900

    refactor: exception 추가

--- src/main/java/com/example/meezy/bc/sharedkernel/exception/ErrorCode.java
+++ src/main/java/com/example/meezy/bc/sharedkernel/exception/ErrorCode.java
@@ -10,6 +10,9 @@ public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러 입니다."),
 
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
+
     EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "로그인 세션이 만료되었습니다."),
     INVALID_JWT(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
 
