27f02be6d87286787799d10458d05320c9bffd29
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-02-23 11:46:06 +0900

    fix: 기존 조회 시 중복 이름 문제 해결

--- src/main/java/com/example/meezy/bc/collaboration/team/application/service/member/QueryTeamMemberService.java
+++ src/main/java/com/example/meezy/bc/collaboration/team/application/service/member/QueryTeamMemberService.java
@@ -1,31 +1,44 @@
 package com.example.meezy.bc.collaboration.team.application.service.member;
 
-import com.example.meezy.bc.sharedkernel.user.CurrentUserQuery;
 import com.example.meezy.bc.collaboration.team.application.service.dto.response.TeamMemberResponse;
 import com.example.meezy.bc.collaboration.team.application.service.exception.TeamNotFoundException;
 import com.example.meezy.bc.collaboration.team.domain.Team;
 import com.example.meezy.bc.collaboration.team.domain.repository.TeamRepository;
+import com.example.meezy.bc.user.user.domain.User;
+import com.example.meezy.bc.user.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class QueryTeamMemberService {
 
     private final TeamRepository teamRepository;
-    private final CurrentUserQuery currentUserQuery;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public List<TeamMemberResponse> query(UUID teamId) {
         Team team = teamRepository.findByTeamId_Value(teamId)
                 .orElseThrow(TeamNotFoundException::new);
 
+        List<UUID> userIds = team.getMembers().stream()
+                .map(m -> m.getUserId().value())
+                .toList();
+
+        Map<UUID, String> nameMap = userRepository.findByUserId_ValueIn(userIds).stream()
+                .collect(Collectors.toMap(
+                        u -> u.getUserId().value(),
+                        User::getName
+                ));
+
         return team.getMembers().stream()
-                .map(t -> TeamMemberResponse.from(t, currentUserQuery.currentUser().name()))
+                .map(m -> TeamMemberResponse.from(m, nameMap.get(m.getUserId().value())))
                 .toList();
     }
 }
